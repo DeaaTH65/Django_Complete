0c70ce8c5fa6b43d4c61cc94b470f484b95e8768
Deaath65 <deaath65@gmail.com>
2023-10-26 12:54:39 +0545

    urls static

--- django_website/urls.py
+++ django_website/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
