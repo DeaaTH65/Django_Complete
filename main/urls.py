733b53a8cd114260fa6f0588f7df483491b9ade8
Deaath65 <deaath65@gmail.com>
2023-10-30 09:24:29 +0545

    userRegistration form

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('<series>', views.series, name='series'),
+    path('<series>/<article>', views.article, name='article'),
 ]
---
+++ users/form.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+
+class userRegistrationForm(UserCreationForm):
+    email = forms.EmailField(help_text='Enter a valid email address', required=True)
+    
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        
+    def save(self, commit=True):
+        user = super(userRegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+            
+        return user
