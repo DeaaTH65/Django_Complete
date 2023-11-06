6e581967c8e074fb00f3fcfac23f817f226419bc
Deaath65 <deaath65@gmail.com>
2023-11-06 10:56:22 +0545

    Account email activated

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path('login/', views.custom_login, name='login'),
     path('logout/', views.custom_logout, name='logout'),
     path('profile/<username>/', views.profile, name='profile'),
-    
-    # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
 ]
