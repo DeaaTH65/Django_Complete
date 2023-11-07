6e275988a5c51dfefd6a2bd90b6ab757a53a9482
Deaath65 <deaath65@gmail.com>
2023-11-07 09:19:10 +0545

    forget password/password reset email

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, SetPasswordForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, SetPasswordForm, PasswordResetForm
 from django.contrib.auth import get_user_model
 from captcha.fields import ReCaptchaField
 from captcha.widgets import ReCaptchaV2Checkbox
@@ -43,3 +43,10 @@ class SetPasswordForm(SetPasswordForm):
     class Meta:
         model = get_user_model()
         fields = ['newpassword1', 'newpassword2']
+        
+        
+class PasswordResetForm(PasswordResetForm):
+    def __init__(self, *args, **kwargs):
+        super(PasswordResetForm, self).__init__(*args, **kwargs)
+        
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
