8ec683fe63b5585debe6a877e699398d9a3460b1
Deaath65 <deaath65@gmail.com>
2023-11-13 09:45:52 +0545

    migrated to PostGres

--- django_website/settings.py
+++ django_website/settings.py
@@ -84,10 +84,14 @@ WSGI_APPLICATION = 'django_website.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'Django_Complete',
+       'USER': 'postgres',
+       'PASSWORD': 'Deaath@65',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
